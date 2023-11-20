61a62cc002fe73856d1877915c1db5b37bb97787
chikugo <yukiyoshi.chikugo@diggle.team>
2023-11-20 20:00:10 +0900

    Fix all delete

--- src/module/delete_all.rs
+++ src/module/delete_all.rs
@@ -1,45 +1,53 @@
-use aws_sdk_dynamodb::{Client};
-use aws_sdk_dynamodb::types::AttributeValue;
+use aws_sdk_dynamodb::{Client, types::AttributeValue};
 use crate::module::error::Error;
 
 pub async fn delete_all_items(client: &Client) -> Result<(), Error> {
-    let table = "books";
-    let resp = client.scan().table_name(table).send().await?;
+    let table_name = "books";
+    let mut exclusive_start_key = None;
 
-    if let Some(items) = resp.items {
-        for item in items {
-            let id_attr = match item.get("id") {
-                Some(attr) => attr,
-                None => {
-                    return Err(Error::unhandled("item id not found".to_string()));
-                }
-            };
-            let id_value = match id_attr {
-                AttributeValue::S(s) => s,
-                _ => return Err(Error::unhandled("id attr error".to_string())),
-            };
-            // println!("Retrieved item id: {:?}", id_value);
+    loop {
+        let resp = client.scan()
+            .table_name(table_name)
+            .set_exclusive_start_key(exclusive_start_key)
+            .send()
+            .await?;
 
-            let sort_attr = match item.get("sort") {
-                Some(attr) => attr,
-                None => {
-                    return Err(Error::unhandled("item sort not found".to_string()));
-                }
-            };
-            let sort_value = match sort_attr {
-                AttributeValue::S(s) => s,
-                _ => return Err(Error::unhandled("sort attr error".to_string())),
-            };
-            // println!("Retrieved item sort: {:?}", sort_value);
+        if let Some(items) = &resp.items {
+            for item in items {
+                let id_attr = match item.get("id") {
+                    Some(attr) => attr,
+                    None => return Err(Error::unhandled("item id not found".to_string())),
+                };
+                let id_value = match id_attr {
+                    AttributeValue::S(s) => s,
+                    _ => return Err(Error::unhandled("id attr error".to_string())),
+                };
 
-            client
-                .delete_item()
-                .table_name(table)
-                .key("id", AttributeValue::S(id_value.clone()))
-                .key("sort", AttributeValue::S(sort_value.clone()))
-                .send()
-                .await?;
-            // println!("Deleted item from table");
+                let sort_attr = match item.get("sort") {
+                    Some(attr) => attr,
+                    None => return Err(Error::unhandled("item sort not found".to_string())),
+                };
+                let sort_value = match sort_attr {
+                    AttributeValue::S(s) => s,
+                    _ => return Err(Error::unhandled("sort attr error".to_string())),
+                };
+
+                client
+                    .delete_item()
+                    .table_name(table_name)
+                    .key("id", AttributeValue::S(id_value.clone()))
+                    .key("sort", AttributeValue::S(sort_value.clone()))
+                    .send()
+                    .await?;
+            }
+        }
+
+        exclusive_start_key = resp.last_evaluated_key.clone();
+
+        println!("Delete count");
+
+        if exclusive_start_key.is_none() {
+            break;
         }
     }
 
