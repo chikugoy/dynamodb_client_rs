b5871b40ee55c2e26da9cf2887e5a57f243d5f9a
chikugo <yukiyoshi.chikugo@diggle.team>
2023-11-24 10:16:42 +0900

    Fix scan all item

--- src/module/scan_all_item.rs
+++ src/module/scan_all_item.rs
@@ -1,13 +1,26 @@
 use aws_sdk_dynamodb::{Client};
 use crate::module::error::Error;
+use std::time::Instant;
 
 pub async fn get_all_items(client: &Client) -> Result<(), Error> {
+    let start = Instant::now();
+
     let resp =
         client.scan().table_name("books").send().await?;
 
-    if let Some(item) = resp.items {
-        println!("Retrieved item: {:?}", item);
+    if let Some(items) = resp.items {
+        let duration = start.elapsed();
+        let execution_time = duration.as_millis();
+        println!("Execution time: {}ms", execution_time);
+
+        for (i, item) in items.iter().enumerate() {
+            println!("Retrieved item {}: {:?}", i + 1, item);
+        }
     }
+
+    let duration = start.elapsed();
+    let execution_time = duration.as_millis();
+    println!("Execution time: {}ms", execution_time);
 
     Ok(())
 }
