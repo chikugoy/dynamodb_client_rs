db7a94102bf494b2e376a3b251c1f4633a7e09d4
chikugo <yukiyoshi.chikugo@diggle.team>
2023-11-25 10:30:34 +0900

    Fix fork join sleep

--- src/module/parallel/fork_join.rs
+++ src/module/parallel/fork_join.rs
@@ -1,9 +1,14 @@
 // Fork-Join
 
-use aws_sdk_dynamodb::{Client};
+// use rand::Rng;
+// use rand::rngs::StdRng;
+// use rand::SeedableRng;
+// use std::sync::{Arc, Mutex};
+// use tokio::time::{sleep, Duration};
 use tokio::task;
 use std::time::Instant;
 use futures::future::join_all;
+use aws_sdk_dynamodb::{Client};
 use crate::module::csv;
 use crate::module::error::Error;
 use crate::module::generate_request;
@@ -15,13 +20,25 @@ pub async fn batch_write_items(client: &Client, item_count: usize) -> Result<(),
     let numbers = (0..item_count).collect::<Vec<_>>();
     let mut tasks = Vec::new();
 
+    // let rng = Arc::new(Mutex::new(StdRng::from_entropy()));
+
     for chunk in numbers.chunks(25).map(|c| c.to_vec()) {
+        // let rng_clone = Arc::clone(&rng);
         let client = client.clone();
         let task = task::spawn(async move {
             let mut requests = Vec::new();
             for i in &chunk {
                 requests.push(generate_request::create_write_request(i.to_string(), "SortKeyValue".to_string()));
             }
+
+            // let number = {
+            //     let mut rng_guard = rng_clone.lock().unwrap();
+            //     let number = rng_guard.gen_range(100..=2000);
+            //     drop(rng_guard);
+            //     number
+            // };
+            //
+            // sleep(Duration::from_millis( number as u64)).await;
 
             client
                 .batch_write_item()
